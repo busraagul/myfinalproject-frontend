765f2679b095e0625aa479bb09b442c2ac044d21
Büşra Gül <busra.sezgin@outlook.com>
2021-03-24 15:04:58 +0300

    Add reactive forms

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup,FormBuilder,FormControl,Validators} from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/services/product.service';
 
 
 @Component({
@@ -11,7 +13,8 @@ export class ProductAddComponent implements OnInit {
   
   productAddForm:FormGroup;
 
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder:FormBuilder,private productService:ProductService,
+    private toastrService:ToastrService) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
@@ -25,5 +28,23 @@ export class ProductAddComponent implements OnInit {
       categoryId:["",Validators.required]
     })
   }
+
+  add(){
+    if (this.productAddForm.valid) {
+      let productModel = Object.assign({},this.productAddForm.value)  
+      this.productService.add(productModel).subscribe(data=>{
+        console.log(data)
+        this.toastrService.success(data.message,"Başarılı")
+      })
+      
+    }
+    else{
+      this.toastrService.error("Formunuz eksik","Dikkat")
+      
+    }
+    
+    
+  }
+
 
 }
