569f14d480eef4ffd98faa2c6fd215f4c4e5dda4
Büşra Gül <busra.sezgin@outlook.com>
2021-03-24 16:26:09 +0300

    Add reactive form

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -32,9 +32,18 @@ export class ProductAddComponent implements OnInit {
   add(){
     if (this.productAddForm.valid) {
       let productModel = Object.assign({},this.productAddForm.value)  
-      this.productService.add(productModel).subscribe(data=>{
-        console.log(data)
-        this.toastrService.success(data.message,"Başarılı")
+      this.productService.add(productModel).subscribe(response=>{
+        
+        this.toastrService.success(response.message,"Başarılı")
+      },responseError=>{
+      if(responseError.error.Errors.length>0){
+       
+        for (let i = 0; i < responseError.error.Errors.length; i++) {
+          
+          this.toastrService.error(responseError.error.Errors[i].ErrorMessage,"Doğrulama hatası")
+        }
+        
+      }   
       })
       
     }
